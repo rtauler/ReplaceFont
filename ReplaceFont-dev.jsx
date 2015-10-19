474e5aae3eeb54ea9fb9b13b786ef167c81ffcf1
rtauler <rtauler@gmail.com>
2015-10-19 21:35:10 +0200

    what happens in case of no outFont, solved

--- ReplaceFont-dev.jsx
+++ ReplaceFont-dev.jsx
@@ -1,11 +1,27 @@
-if(app.documents.length != 0){
-    var doc = app.activeDocument;
+var doc = app.activeDocument;
     
     var inFont = prompt("In Font?");
+    
     var outFont = prompt("Out Font?");
+    if (outFont === ""){
+        outFont = inFont
+    }
+    else if (outFont){
+    }
+
     var outSize = prompt("Wich Size?");
+    if (outSize === ""){
+        outSize = inSize
+    }
+    else if (outSize){
+    }
+
     var outColor = new SolidColor;
     outColor.rgb.hexValue = prompt("Wich outColor?")
+
+
+
+if(app.documents.length != 0){
 
     for(i = 0; i < doc.artLayers.length; ++i){
         var layer = doc.artLayers[i];
