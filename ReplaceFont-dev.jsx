22fc6fe4158521e745f1c8ecd4071d8a9cd40707
rtauler <rtauler@gmail.com>
2015-10-19 20:26:03 +0200

    prompts added

--- ReplaceFont-dev.jsx
+++ ReplaceFont-dev.jsx
@@ -1,13 +1,16 @@
 if(app.documents.length != 0){
     var doc = app.activeDocument;
- 
+ 	
+    var inFont = prompt("In Font?");
+	var outFont = prompt("Out Font?");
+
     for(i = 0; i < doc.artLayers.length; ++i){
         var layer = doc.artLayers[i];
  
-        if(layer.textItem.font == "ArialMT")  
+        if(layer.textItem.font == inFont)  
         {
 
-            layer.textItem.font = "ComicSansMS";
+            layer.textItem.font = outFont;
             
         }
     }
