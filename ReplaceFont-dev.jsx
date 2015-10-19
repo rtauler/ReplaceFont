cd3489bbae47f832d881598f415da2e2d4691813
rtauler <rtauler@gmail.com>
2015-10-19 19:08:55 +0200

    discrimination acquired

--- ReplaceFont-dev.jsx
+++ ReplaceFont-dev.jsx
@@ -4,7 +4,9 @@ if(app.documents.length != 0){
     for(i = 0; i < doc.artLayers.length; ++i){
         var layer = doc.artLayers[i];
  
-        if((layer.kind == LayerKind.TEXT) || (textItem.SolidColor)) {
+        if(layer.textItem.font == "ArialMT")  
+        {
+
             layer.textItem.font = "ComicSansMS";
             
         }
