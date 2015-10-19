95bf5bd02379443633b3074b01b3c2b25354c892
rtauler <rtauler@gmail.com>
2015-10-19 22:48:16 +0200

    outfont broken, new organization of code, few lines that are useless, trying to work with the groups

--- ReplaceFont-dev.jsx
+++ ReplaceFont-dev.jsx
@@ -1,38 +1,40 @@
-var doc = app.activeDocument;
-    
-    var inFont = prompt("In Font?");
-    
+var inFont = prompt("In Font?");
+
+var inSize = prompt("Wich inSize");
+if (inSize === ""){
+    layer.texItem.size == inSize 
+}
+else if (inSize){}
+
     var outFont = prompt("Out Font?");
-    if (outFont === ""){
-        outFont = inFont
-    }
-    else if (outFont){
-    }
+if (outFont === ""){
+    outFont == inFont
+}
+else if (outFont){}
 
     var outSize = prompt("Wich Size?");
-    if (outSize === ""){
-        outSize = inSize
-    }
-    else if (outSize){
-    }
+if (outSize === ""){
+    outSize == inSize
+}
+else if (outSize){}
 
     var outColor = new SolidColor;
-    outColor.rgb.hexValue = prompt("Wich outColor?")
+outColor.rgb.hexValue = prompt("Wich outColor?")
 
 
-
+var doc = app.activeDocument;
 if(app.documents.length != 0){
 
     for(i = 0; i < doc.artLayers.length; ++i){
         var layer = doc.artLayers[i];
- 
-        if((layer.kind == LayerKind.TEXT) && (layer.textItem.font == inFont))  
+
+        if((layer.kind == LayerKind.TEXT) && (layer.textItem.font == inFont) && (layer.textItem.size == inSize))  
         {
 
             layer.textItem.font = outFont;
             layer.textItem.size = outSize+"pt";
             layer.textItem.color = outColor;
-            
+
         }
     }
 }
