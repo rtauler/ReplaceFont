8dd2d0929a833fe83f62590ed52aeb14dc679aea
rtauler <rtauler@gmail.com>
2015-10-19 21:04:38 +0200

    size and color added

--- ReplaceFont-dev.jsx
+++ ReplaceFont-dev.jsx
@@ -1,17 +1,25 @@
 if(app.documents.length != 0){
     var doc = app.activeDocument;
- 	
+    
     var inFont = prompt("In Font?");
-	var outFont = prompt("Out Font?");
+    var outFont = prompt("Out Font?");
+    var outSize = prompt("Wich Size?");
+    var outColor = new SolidColor;
+    outColor.rgb.hexValue = prompt("Wich outColor?")
 
     for(i = 0; i < doc.artLayers.length; ++i){
         var layer = doc.artLayers[i];
  
-        if(layer.textItem.font == inFont)  
+        if((layer.kind == LayerKind.TEXT) && (layer.textItem.font == inFont))  
         {
 
             layer.textItem.font = outFont;
+            layer.textItem.size = outSize+"pt";
+            layer.textItem.color = outColor;
             
         }
     }
 }
+
+
+// && (layer.textItem.size == inSize)
